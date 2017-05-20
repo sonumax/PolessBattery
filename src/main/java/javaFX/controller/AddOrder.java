44ac0dce49c1fe005c05015030f3a7a76eaa147e
sonumax <sonumax2@gmail.com>
2017-05-20 12:18:40 +0300

    Add in class change order action

--- src/main/java/javaFX/controller/AddOrder.java
+++ src/main/java/javaFX/controller/AddOrder.java
@@ -9,6 +9,7 @@ import hibernate.tables.CustomersEntity;
 import hibernate.tables.OrdersEntity;
 import hibernate.tables.PolarityEntity;
 import javaFX.view.RecordOder;
+import javaFX.view.WorkTableOrders;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,7 +24,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import util.DialogManager;
+import util.Utils;
 
 import java.io.IOException;
 import java.sql.Date;
@@ -44,26 +45,10 @@ public class AddOrder {
     public Button btnCancel;
 
     private Stage orderStage;
-
     private RecordOder recordOder;
-
-    public void setRecordOder(RecordOder recordOder) {
-        this.recordOder = recordOder;
-    }
 
     @FXML
     private void initialize() {
-        if (recordOder != null) {
-            chbCustomers.setValue(recordOder.getOrganizationName());
-            chbMark.setValue(recordOder.getMark());
-            chbCapacity.setValue(recordOder.getCapacity());
-            chbAmperage.setValue(recordOder.getAmperage());
-            chbPolarity.setValue(recordOder.getNamePolarity());
-            txtfQuantity.setText(String.valueOf(recordOder.getQuantityProduct()));
-
-            Date date = recordOder.getDateExecution();
-            dateDateExecution.setAccessibleText(date.toString());
-        }
         fillChoiceBox();
     }
 
@@ -80,11 +65,14 @@ public class AddOrder {
             String fxmlFile = "/fxml/addCustomer.fxml";
             FXMLLoader loader = new FXMLLoader();
             Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
+            AddCustomer customerController = loader.getController();
+            customerController.setOrderStage(orderStage);
             stage.setTitle("Новый заказчик");
             stage.setResizable(false);
             stage.setScene(new Scene(root));
             stage.initModality(Modality.WINDOW_MODAL);
             stage.initOwner(orderStage);
+            orderStage.hide();
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -97,11 +85,14 @@ public class AddOrder {
             String fxmlFile = "/fxml/addBattery.fxml";
             FXMLLoader loader = new FXMLLoader();
             Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
+            AddBattery batteryController = loader.getController();
+            batteryController.setOrderStage(orderStage);
             stage.setTitle("Новый заказчик");
             stage.setResizable(false);
             stage.setScene(new Scene(root));
             stage.initModality(Modality.WINDOW_MODAL);
             stage.initOwner(orderStage);
+            orderStage.hide();
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -111,49 +102,72 @@ public class AddOrder {
     public void actionClose(ActionEvent actionEvent) {
         Node source = (Node) actionEvent.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
-        stage.close();
+        stage.hide();
     }
 
     public void actionSave(ActionEvent actionEvent) {
         if (!checkValues()){
-            DialogManager.showInfoDialog("Ошибка", "Заполните все поля");
             return;
         }
         OrderDAO orderDAO = new OrderDAO();
+        if (recordOder == null) {
+            OrdersEntity ordersEntity = getOrdersEntityWithSetFields(new OrdersEntity());
+            orderDAO.add(ordersEntity);
+        } else {
+            OrdersEntity oldOrder = orderDAO.getOrderById(recordOder.getOrderId());
+            OrdersEntity newOrder = getOrdersEntityWithSetFields(oldOrder);
+            orderDAO.update(newOrder);
+        }
+        WorkTableOrders.fillTablesItem();
+        actionClose(actionEvent);
+    }
+
+    private OrdersEntity getOrdersEntityWithSetFields(OrdersEntity ordersEntity) {
         CustomerDAO customerDAO = new CustomerDAO();
         CustomersEntity inputCustomer = customerDAO.getOrdersByOrganizationName(chbCustomers.getValue());
+        BatteryEntity inputBattery = getBatteryEntity();
+        Calendar calendar = getCalendar();
+        int quantity = Integer.valueOf(txtfQuantity.getText());
+        return getOrdersEntityWithSetFields(ordersEntity,inputCustomer, inputBattery, calendar, quantity);
+    }
 
+    private BatteryEntity getBatteryEntity() {
         String mark = chbMark.getValue();
         int capacity = chbCapacity.getValue();
         int amperage = chbAmperage.getValue();
-        String polarityId = chbPolarity.getValue();
-        int quantity = Integer.valueOf(txtfQuantity.getText());
-
-        LocalDate date = dateDateExecution.getValue();
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_MONTH, date.getDayOfMonth());
-        cal.set(Calendar.MONTH, date.getMonthValue());
-        cal.set(Calendar.YEAR, date.getYear());
+        String polarityName = chbPolarity.getValue();
 
         PolarityDAO polarityDAO = new PolarityDAO();
-        PolarityEntity polarity = polarityDAO.getByName(polarityId);
+        PolarityEntity polarity = polarityDAO.getByName(polarityName);
 
         BatteryDAO batteryDAO = new BatteryDAO();
-        BatteryEntity inputBattery = batteryDAO.getByMarkCapacityAmperagePolarity(mark, capacity, amperage, polarity.getPolarityId());
+        return batteryDAO.getByMarkCapacityAmperagePolarity(mark, capacity, amperage, polarity.getPolarityId());
+    }
 
-        OrdersEntity ordersEntity = new OrdersEntity();
+    private Calendar getCalendar() {
+        LocalDate date = dateDateExecution.getValue();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, date.getDayOfMonth());
+        calendar.set(Calendar.MONTH, date.getMonthValue() - 1);
+        calendar.set(Calendar.YEAR, date.getYear());
+        return calendar;
+    }
+
+    private OrdersEntity getOrdersEntityWithSetFields(OrdersEntity ordersEntity,CustomersEntity inputCustomer, BatteryEntity inputBattery, Calendar calendar, int quantity) {
         ordersEntity.setCustomer(inputCustomer);
         ordersEntity.setBattery(inputBattery);
         ordersEntity.setQuantityProduct(quantity);
-        ordersEntity.setDateExecution(new Date(cal.getTimeInMillis()));
-
-        orderDAO.add(ordersEntity);
-
-        actionClose(actionEvent);
+        ordersEntity.setDateExecution(new Date(calendar.getTimeInMillis()));
+        return ordersEntity;
     }
 
     public void setOrderStage(Stage orderStage) {
         this.orderStage = orderStage;
+    }
+
+    public void setRecordOder(RecordOder recordOder) {
+        this.recordOder = recordOder;
+        setFields();
     }
 
     private void fillChoiceBox() {
@@ -200,11 +214,44 @@ public class AddOrder {
     }
 
     private boolean checkValues() {
-        if (chbCustomers == null || chbMark == null
-                || chbCapacity == null || chbAmperage == null || chbPolarity == null
-                || txtfQuantity.getText().trim().length() == 0){
+        if (Utils.countLengthTextField(txtfQuantity) == 0){
+            Utils.showErrorDialog("Ошибка", "Заполните все поля");
             return false;
         }
         return true;
+    }
+
+    private void setFields() {
+        if (recordOder == null) {
+            setFirstItem();
+            cleanTextField();
+            return;
+        }
+        setFieldsOfOrder();
+    }
+
+    private void cleanTextField() {
+        txtfQuantity.setText("");
+        dateDateExecution.setValue(LocalDate.now());
+    }
+
+    private void setFieldsOfOrder() {
+        chbCustomers.getSelectionModel().select(recordOder.getOrganizationName());
+        chbMark.getSelectionModel().select(recordOder.getMark());
+        chbCapacity.getSelectionModel().select(Integer.valueOf(recordOder.getCapacity()));
+        chbAmperage.getSelectionModel().select(Integer.valueOf(recordOder.getAmperage()));
+        chbPolarity.getSelectionModel().select(recordOder.getNamePolarity());
+        txtfQuantity.setText(String.valueOf(recordOder.getQuantityProduct()));
+
+        Date date = recordOder.getDateExecution();
+        dateDateExecution.setValue(date.toLocalDate());
+    }
+
+    private void setFirstItem() {
+        chbMark.getSelectionModel().selectFirst();
+        chbCapacity.getSelectionModel().selectFirst();
+        chbAmperage.getSelectionModel().selectFirst();
+        chbCustomers.getSelectionModel().selectFirst();
+        chbPolarity.getSelectionModel().selectFirst();
     }
 }

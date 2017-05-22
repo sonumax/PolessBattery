90b0950a4d60944ef2aeac1200f84c9b3305bc37
sonumax <sonumax2@gmail.com>
2017-05-22 13:57:54 +0300

    Delete info dialog

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -4,14 +4,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class Utils {
-    public static void showInfoDialog(String title, String text){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setContentText(text);
-        alert.setHeaderText("");
-        alert.showAndWait();
-    }
-
     public static void showErrorDialog(String title, String text){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
